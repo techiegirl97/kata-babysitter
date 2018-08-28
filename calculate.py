351823c5da2c33d4363ae3a2fadf4abe48b59936
Christina Fleming <techiegirl97@gmail.com>
2018-08-28 15:04:00 -0400

    Add files via upload

--- calculate.py
+++ calculate.py
@@ -3,22 +3,28 @@ def calculate(start,stop,bed):
 	#import the needed libraries	
 	import math
 	
+	timescale = {5,6,7,8,9,10,11,12,1,2,3,4}
+	#do position minus position to get hours
+	
 	#round the start\stop time to the nearest hour
 	#bedtime may be fluid, round to the next hour
-	
-	#write the functions for special times
 
+	#the main function to add all earnings, will call the others
+	#add up the earnings
+
+	#the first time block
 	#calculate the hours from start to bed
 	#5 to 8 is 3 hours at ($12/hr) = $36
 	
+	#the second time block
 	#calculate the hours from bed to midnight
 	#8 to mid is 4 hours at ($8/hr) = $32
 	
+	#the third time block
 	#calculate the hours from midnight to stop time
 	#mid to 4 is 4 hours at ($16/hr) = $64
 	
-	#add up the earnings
-	#36+32+64 = $132
+
 
 	
 #write the tests
