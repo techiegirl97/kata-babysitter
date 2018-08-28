70ca0b6dc792c0dbcf0076cb58fc8f564e5627a1
Christina Fleming <techiegirl97@gmail.com>
2018-08-28 14:40:41 -0400

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,18 @@
+def calculate(start,stop,bed):
+	
+	#import the needed libraries	
+	import math
+	
+	#round the start\stop time to the nearest hour
+	#bedtime may be fluid, round to the next hour
+	
+	#write the functions for special times
+	
+	#add up the earnings
+
+	
+#write the tests
+#order of variables (start time, stop time, bedtime)
+test.expect(calculate(5,4,8) == 0)
+test.expect(calculate(5,4,7) == 0)
+
